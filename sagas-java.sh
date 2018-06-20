9e2849453cd438b01dc6b809284f73609d65c082
Samlet Wu <xiaofei.wu@gmail.com>
2018-06-20 09:10:18 +0800

    init version

---
+++ sagas-java.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install openjdk-8-jdk maven gradle
+
---
+++ sagas-node.sh
@@ -0,0 +1,12 @@
+#!/binb/bash
+curl -fsSL get.docker.com | sh
+
+# Restart docker to make sure we get the latest version of the daemon if there is an upgrade
+sudo service docker restart
+# Make sure we can actually use docker as the vagrant user
+sudo usermod -aG docker vagrant
+
+# docker-compose
+sudo apt-get install python-pip
+sudo pip install docker-compose
+
