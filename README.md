73b9f5a95a7c03c03917cda66738d26b5a83e6d4
Samlet Wu <xiaofei.wu@gmail.com>
2018-06-20 09:22:27 +0800

    add startup script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sagas
+
 abc sagas IaC startup scripts
+
+## startup
+
+```sh
+curl -fsSL https://raw.githubusercontent.com/samlet/sagas/master/sagas-start.sh | bash
+```
---
+++ sagas-start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+if [ $# -lt 1 ]; then	
+	echo "assign a command, available commands: init, install, run, ..."
+	exit -1
+fi
+
+CMD=$1
+case "$CMD" in
+	"node")		
+		curl -fsSL https://raw.githubusercontent.com/samlet/sagas/master/sagas-node.sh | bash
+		;;
+	"java")
+		curl -fsSL https://raw.githubusercontent.com/samlet/sagas/master/sagas-java.sh | bash
+		;;	
+	"run")
+		if [ $# -gt 1 ]; then	
+			target=$2
+			echo "run $target ..."
+		else
+			echo "use: ...."
+		fi
+		;;
+	*)
+        echo "➽ execute script $CMD ..."
+		curl -fsSL https://raw.githubusercontent.com/samlet/sagas/master/sagas-${CMD}.sh | bash
+		;;
+esac
+
